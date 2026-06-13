8f878f1ecd381a6b3549761f4cc2037f160412fe
Eleazar Pequeno <edpu0033@gmail.com>
2026-06-13 03:23:16 -0500

    fix: add explicit contentWindowInsets to Scaffold for system bars

--- app/src/main/java/com/juiceroll/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/juiceroll/ui/screens/home/HomeScreen.kt
@@ -2,11 +2,13 @@ package com.juiceroll.ui.screens.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -112,6 +114,7 @@ fun HomeScreen(
     }
 
     Scaffold(
+        contentWindowInsets = WindowInsets.systemBars,
         topBar = {
             TopAppBar(
                 title = {
